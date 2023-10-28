00ae0c32ca481aeeb58106d74e4107ad0883fbb6
Rohan Ahmed <148573695+justrealrohan@users.noreply.github.com>
2023-10-28 19:08:56 +0600

    Add files via upload

---
+++ list.dart
@@ -0,0 +1,11 @@
+void main() {
+  // list
+  var friends = ['rohan', 'sarwar', 'tahmid',];
+  friends.add('jems');
+  print(friends);
+  print(friends[0]);
+
+  var ageOfFriends = [27, 28, 28];
+  ageOfFriends.add(28);
+  print(ageOfFriends);
+}
