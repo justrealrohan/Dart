27ed7ace84363b2bc656363b10bc631498ec778a
Rohan Ahmed <148573695+justrealrohan@users.noreply.github.com>
2023-10-28 19:01:29 +0600

    Add files via upload

---
+++ variables_oparators_and_typecasting.dart
@@ -0,0 +1,44 @@
+void main() {
+
+  var firstName = 'Rohan';
+  var lastName = 'Ahmed';
+  var age = 27;
+
+  print(firstName + ' ' + lastName);
+  print(age);
+
+  String nickName = "Bruce";
+  print(firstName + ' ' + lastName + ' ' + 'and nickname is' + ' ' + nickName);
+
+  // integer
+  const age2 = 29;
+  print(age2);
+
+  // double
+  double sscGpa = 4.69;
+  print(sscGpa);
+
+  // boolean for true / false
+  var ans = false;
+  print(ans);
+
+  //operators
+  var number1 = 82;
+  var number2 = 10;
+
+  var sum = number1 + number2;
+  print(sum);
+
+  var subtract = number1 - number2;
+  print(subtract);
+
+  var multiply = number1 * number2;
+  print(multiply);
+
+  // type casting
+  var typeCasting1 = (number1 / number2).toInt();
+  print(typeCasting1);
+
+  var age3 = (24 - 1.08).toString();
+  print(age3);
+}
