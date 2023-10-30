db373dc537e2e74161348e94e191986269381b01
Rohan Ahmed <148573695+justrealrohan@users.noreply.github.com>
2023-10-30 23:10:20 +0600

    Add files via upload

---
+++ 5.1_loop_list.dart
@@ -0,0 +1,14 @@
+void main() {
+  List varsityName = ['SUB', 'UAP', 'AUST', 'NSU'];
+
+  varsityName.forEach((element){
+    print(element);
+  });
+
+  print("\nLine GAP\n");
+
+  // for loop
+  for (var value in varsityName) {
+    print(value + '\nSEU');
+  }
+}
